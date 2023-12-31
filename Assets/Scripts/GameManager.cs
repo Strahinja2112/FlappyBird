529ab47d4120fac1cedc9e4b048990978ee87e1c
Strahinja <111398879+Strahinja2112@users.noreply.github.com>
2024-01-01 00:10:33 +0100

    first commit

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,17 +1,27 @@
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
-  private int score = 0;    
+  private int score = 0;  
+  private bool gameOver = false;
+  public GameObject image;  
+  public TextMeshProUGUI scoreText;  
+
+  private void Start() {
+    Time.timeScale = 0;
+  }
 
   public void IncreaseScore() {
     score++;
+    scoreText.text = score.ToString();
   }
 
   public void GameOver() {
-    Debug.Log("GAME OVER");  
+    image.SetActive(true);
+    gameOver = true;
   }
 
-  private void Start() {
-
+  public void OnStart() {
+    Time.timeScale = 1;
   }
 }
