695d3c9059ac08c1df293debcc5a0b35d307d45f
Strahinja <111398879+Strahinja2112@users.noreply.github.com>
2023-12-31 19:36:17 +0100

    added basic gravity

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class Player : MonoBehaviour {
+  public float gravity = -9.8f;
+  [SerializeField]
+  private float strength = 5f;
+
+  private Vector3 direction;
+
+  private void Update() {
+    // 0 je LMB, 1 MMB, 2 RMB
+    if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) {
+      direction = Vector3.up * strength;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    // mobile touch
+    if (Input.touchCount > 0) {
+      Touch touch = Input.GetTouch(0);
+
+      if (touch.phase == TouchPhase.Began) {
+        direction = Vector3.up * strength;
+      }
     }
+
+    direction.y += gravity * Time.deltaTime;
+    transform.position += direction * Time.deltaTime;
+  }
 }
